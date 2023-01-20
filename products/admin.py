a2a730f29f496846ca71fcdb6174d2ee9ab4074c
Mehmet Kabasakal <sehriistanbul@gmail.com>
2023-01-20 18:06:45 +0300

    display, editable, filter added to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_date", "is_in_stock", "update_date")
+    list_editable = ( "is_in_stock", )
+    # list_display_links = ("create_date", ) # default olarak listin ilki link gelir. hem link hem editable olmaz
+    list_filter = ("is_in_stock", "create_date")
+
+admin.site.register(Product, ProductAdmin)
 
 admin.site.site_title = "Coredinat Title"
 admin.site.site_header = "Coredinat Admin Portal"  
