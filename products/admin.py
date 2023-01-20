3143559313a2c34aa869285683e24e4a63e26d56
Mehmet Kabasakal <sehriistanbul@gmail.com>
2023-01-21 02:01:11 +0300

    added actions def

--- products/admin.py
+++ products/admin.py
@@ -6,6 +6,33 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ( "is_in_stock", )
     # list_display_links = ("create_date", ) # default olarak listin ilki link gelir. hem link hem editable olmaz
     list_filter = ("is_in_stock", "create_date")
+    ordering = ("name",) 
+    search_fields = ("name",)
+    prepopulated_fields = {'slug' : ('name',)} #nameden slug üretiyor
+    list_per_page = 25
+    date_hierarchy = "update_date"
+    # fields = (('name', 'slug'), 'description', "is_in_stock") #fieldset kullandığımız zaman bunu kullanamayız
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('name', 'slug'), "is_in_stock" # to display multiple fields on the same line, wrap those fields in their own tuple
+            ),
+            # 'classes': ('wide', 'extrapretty'), wide or collapse
+        }),
+        ('Optionals Settings', {
+            "classes" : ("collapse", ),
+            "fields" : ("description",),
+            'description' : "You can use this section for optionals settings"
+        })
+    )
+    actions = ("is_in_stock", )
+
+    def is_in_stock(self, request, queryset):
+        count = queryset.update(is_in_stock=True)
+        self.message_user(request, f"{count} kinds of product added to stocks")
+    is_in_stock.short_description = 'Add selected products to stock' # bu şekilde tanımlama yaptık, adminde daha mantıklı görünmesi için
+
+    
 
 admin.site.register(Product, ProductAdmin)
 
